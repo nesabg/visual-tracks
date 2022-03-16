4b9199adf654948115f25fa67f2929f990411d22
Asen Asenov <53828105+nesabg@users.noreply.github.com>
2022-03-16 22:14:48 +0200

    Fix: refactoring rendering and logical tracks

--- src/context/TracksContext.js
+++ src/context/TracksContext.js
@@ -3,27 +3,10 @@ import React, { useState } from 'react'
 export const TrackContext = React.createContext()
 
 const TracksContextProvider = (props) => {
+    
     const [rawData, setRawData] = useState([])
-    const [tracks, setTracks] = useState([])
-    const [tracksToRender, setTracksToRender] = useState([])
 
     const authCookie = document.cookie.length > 0 ? document.cookie.split('; ').find(e => e.includes('track-auth')).split('=')[1] : null   
-
-    const formatData = (arr) => {
-        const newArr = []
-        arr.map(e => {
-            if(newArr[e.ip] === undefined){
-                return newArr[e.ip] = [e];
-            }else{
-                return newArr[e.ip].push(e)
-            }
-        })
-
-        const finalData = Object.entries(newArr)
-
-        setTracks(finalData)
-        setTracksToRender(tracks.slice(0, 10))
-    }
 
     const getData = async () => {
         try{
@@ -33,44 +16,34 @@ const TracksContextProvider = (props) => {
                     Authorization: `Bearer ${authCookie}`
                 }
             })
+
             const data = await res.json()
+            const parsedData = JSON.parse(data)
 
-            setRawData(JSON.parse(data))
-
-            formatData(rawData)
-
+            setRawData(parsedData)
         }catch(e){
             console.error('Err from try/catch', e);
-        
         }
+        
     }
 
-    const deleteTrack = async (id) => {
-        try{
 
-            setRawData(p => rawData.filter(t => t._id !== id))            
-            formatData(rawData)
-            
-            await fetch(`http://localhost:3001/api/delete/${id}`,{
+    const deleteTrack = (id) => {
+        setRawData(prevState => prevState.filter(t => t._id !== id))   
+        try{          
+            fetch(`http://localhost:3001/api/delete/${id}`,{
                 method: 'DELETE',
                 headers: {
                     Authorization: `Bearer ${authCookie}`
                 }
-            })
-            
-        } catch(e){
+            })            
+        }catch(e){
             console.error(e)
-        }
-
-    }
-
-    const changeTracksToRender = (page) => {
-        const coef = page * 20
-        setTracksToRender(tracks.slice(coef, coef + 10))
+        }      
     }
 
     return(
-        <TrackContext.Provider value={{getData, tracks, tracksToRender, changeTracksToRender, setTracksToRender, deleteTrack}}>
+        <TrackContext.Provider value={{getData, rawData, deleteTrack, }}>
             {props.children}
         </TrackContext.Provider>
     )
